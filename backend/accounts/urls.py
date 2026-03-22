45a60473c2c50faa536d41ad193b4ff1a243a3c0
Sonu445Kum <sonuroy1629@gmail.com>
2026-03-22 23:31:22 +0530

    write urls for the accounts

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,17 +1,11 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.urls import path
+from accounts.views.auth_views import RegisterView, LoginView
+from accounts.views.profile_views import ProfileView
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    is_verified = models.BooleanField(default=False)
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 
-    # NEW FIELDS
-    name = models.CharField(max_length=255, blank=True)
-    bio = models.TextField(blank=True)
-    avatar = models.URLField(blank=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.email
+    # NEW
+    path('profile/', ProfileView.as_view()),
+]
